6ec4c4b22a71d27d2c0edbf5f998853f4c79b50f
omarkfrank <omarkfrank@yahoo.com>
2024-05-23 10:46:20 -0300

    Project created

---
+++ src/test/java/com/mark/projeto/cursoSpringBoot/CursoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mark.projeto.cursoSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
